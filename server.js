07d81ddce27e01da42877f55e1ef4415250e0f43
Aleph Cadena <alephcadena@gmail.com>
2021-11-28 17:05:51 -0600

    added post

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json ');
+const db = require('./db/db.json ');
+const fs = require('fs');
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.port || 3001;
 
@@ -17,6 +19,42 @@ app.get('/notes', (req, res) =>
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/api/notes', (req, res) =>
+  console.log(db),
+  res.status(200).json(db)
+),
+
+app.post('/api/notes', (req, res) => {
+
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const Notes = {
+            title,
+            text,
+            id: uuidv4()
+        }
+
+        db.push(Notes)
+        const arrStr = JSON.stringify(db, null, 2)
+        
+        fs.writeFile('./db/db.json', arrStr, (err) =>
+        err
+            ? console.error(err)
+            : console.log('new note succesfully created')
+        );
+
+        const response = {
+            status: 'success',
+            body: Notes,
+        };
+
+        res.status(200).json(response);
+    } else{
+        res.status(500).json('Something went wrong with posting new note');
+    }
+});
 
 app.listen(PORT, () => 
   console.log(`App listening on: ${PORT}`)
